ad5baa527f21702dc66314fac8943d190206f604
Sekarraja G K <113578726+Sekarraja23@users.noreply.github.com>
2024-01-05 16:05:49 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+        Email em1 = new Email("John", "Smith");
+        System.out.println(em1.showInfo());
+    }
+}
